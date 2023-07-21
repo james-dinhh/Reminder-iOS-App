be60f2053719874900bece0f9caef5c52b6cb4b5
Tin Dinh <jamesdinh.bib@gmail.com>
2023-07-21 15:13:23 +0700

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Dinh Le Hong Tin on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
